6eac9d5cb44f9ab7a327de6d7496000d6a918ab2
HanHyunSoo <gustn4563@gmail.com>
2022-07-05 23:42:54 +0900

    Start Project

---
+++ src/main/java/com/debug/DebugWebApplication.java
@@ -0,0 +1,13 @@
+package com.debug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebugWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebugWebApplication.class, args);
+    }
+
+}
