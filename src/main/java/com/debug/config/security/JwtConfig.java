30a0b4176fd30af6d845848133978d098daa9947
HanHyunSoo <gustn4563@gmail.com>
2022-07-15 10:30:25 +0900

    feat : Add Jwt Configuration

---
+++ src/main/java/com/debug/config/security/JwtConfig.java
@@ -0,0 +1,18 @@
+package com.debug.config.security;
+
+import com.debug.oauth.token.AuthTokenProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JwtConfig {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Bean
+    public AuthTokenProvider jwtProvider() {
+        return new AuthTokenProvider(secret);
+    }
+}
