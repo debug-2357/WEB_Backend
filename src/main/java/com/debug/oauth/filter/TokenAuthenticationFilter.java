891efd9cf5eee3ef7c07825c4c30478cf150fd1f
HanHyunSoo <gustn4563@gmail.com>
2022-07-15 10:48:35 +0900

    feat : Add Custom Token handler, filter and exception

---
+++ src/main/java/com/debug/oauth/filter/TokenAuthenticationFilter.java
@@ -0,0 +1,39 @@
+package com.debug.oauth.filter;
+
+import com.debug.oauth.token.AuthToken;
+import com.debug.oauth.token.AuthTokenProvider;
+import com.debug.util.HeaderUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
+    private final AuthTokenProvider authTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        String tokenString = HeaderUtil.getAccessToken(request);
+        AuthToken token = authTokenProvider.convertAuthToken(tokenString);
+
+        if (token.validate()) {
+            Authentication authentication = authTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
