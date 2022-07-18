d1cfc4ed6d23163758f29986730f36367b3b7706
HanHyunSoo <gustn4563@gmail.com>
2022-07-19 00:36:51 +0900

    feat : Add Custom Exception(OAuth2ProviderMissMatchException)

---
+++ src/main/java/com/debug/oauth/exception/OAuth2ProviderMissMatchException.java
@@ -0,0 +1,8 @@
+package com.debug.oauth.exception;
+
+public class OAuth2ProviderMissMatchException extends RuntimeException {
+
+    public OAuth2ProviderMissMatchException(String message) {
+        super(message);
+    }
+}
