5409a7e2059fb4250243300e284e6deb31edc55a
HanHyunSoo <gustn4563@gmail.com>
2022-07-31 04:55:57 +0900

    fix : Add Configuration annotation

--- src/main/java/com/debug/config/properties/CorsProperties.java
+++ src/main/java/com/debug/config/properties/CorsProperties.java
@@ -3,9 +3,11 @@ package com.debug.config.properties;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Setter
+@Configuration
 @ConfigurationProperties(prefix = "cors")
 public class CorsProperties {
     private String allowedOrigins;
