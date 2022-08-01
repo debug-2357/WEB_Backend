ac103eefdd0d3931e4680e6fd3d7eadef6c18626
HanHyunSoo <gustn4563@gmail.com>
2022-08-01 23:32:52 +0900

    feat : Add value to MessageEnum

--- src/main/java/com/debug/common/MessageEnum.java
+++ src/main/java/com/debug/common/MessageEnum.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 public enum MessageEnum {
 
     OK("요청 성공"),
-    CREATED("생성 작업 성공");
+    CREATED("생성 작업 성공"),
+    UNMATCHED_ID_OR_PASSWORD("아이디 또는 비밀번호가 일치하지 않습니다.");
 
     final String message;
 
