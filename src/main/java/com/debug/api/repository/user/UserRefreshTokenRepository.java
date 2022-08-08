ebe4084b6507dbdfc16f916da9b9385a3dee4d5b
HanHyunSoo <gustn4563@gmail.com>
2022-08-08 16:55:31 +0900

    feat : Convert return variable to Optional

--- src/main/java/com/debug/api/repository/user/UserRefreshTokenRepository.java
+++ src/main/java/com/debug/api/repository/user/UserRefreshTokenRepository.java
@@ -1,9 +1,12 @@
 package com.debug.api.repository.user;
 
+import com.debug.api.entity.user.User;
 import com.debug.api.entity.user.UserRefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRefreshTokenRepository extends JpaRepository<UserRefreshToken, Long> {
-    UserRefreshToken findByUserId(String userId);
-    UserRefreshToken findByUserIdAndRefreshToken(String userId, String refreshToken);
+    Optional<UserRefreshToken> findByUser(User user);
+    Optional<UserRefreshToken> findByRefreshTokenAndUser(String refreshToken, User user);
 }
