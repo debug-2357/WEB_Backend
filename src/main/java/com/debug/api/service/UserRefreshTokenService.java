d6d9df15fd8c468fb808c8591b221bbe0eb5d709
HanHyunSoo <gustn4563@gmail.com>
2022-08-09 02:53:01 +0900

    feat : Add User Refresh Token Service and User Response

---
+++ src/main/java/com/debug/api/service/UserRefreshTokenService.java
@@ -0,0 +1,62 @@
+package com.debug.api.service;
+
+import com.debug.api.entity.user.User;
+import com.debug.api.entity.user.UserRefreshToken;
+import com.debug.api.exception.RefreshTokenNotFoundException;
+import com.debug.api.exception.UserNotFoundException;
+import com.debug.api.repository.user.UserRefreshTokenRepository;
+import com.debug.api.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserRefreshTokenService {
+
+    private final UserRepository userRepository;
+    private final UserRefreshTokenRepository userRefreshTokenRepository;
+
+    @Transactional(readOnly = true)
+    public UserRefreshToken findByUserId(String userId) {
+        User user = userRepository.findByUserId(userId).orElseThrow(
+                UserNotFoundException::new
+        );
+        return userRefreshTokenRepository.findByUser(user).orElseThrow(
+                RefreshTokenNotFoundException::new
+        );
+
+    }
+
+    @Transactional(readOnly = true)
+    public UserRefreshToken findByRefreshTokenAndUserId(String refreshToken, String userId) {
+        User user = userRepository.findByUserId(userId).orElseThrow(
+                UserNotFoundException::new
+        );
+
+        return userRefreshTokenRepository.findByRefreshTokenAndUser(refreshToken, user).orElseThrow(
+                RefreshTokenNotFoundException::new
+        );
+    }
+
+    @Transactional
+    public void save(String refreshToken, String userId) {
+        User user = userRepository.findByUserId(userId).orElseThrow(
+                UserNotFoundException::new
+        );
+        UserRefreshToken userRefreshToken = userRefreshTokenRepository.findByUser(user).orElse(null);
+
+        if (userRefreshToken != null) {
+            // 리프래쉬 토큰이 db에 존재하면 업데이트
+            userRefreshToken.updateRefreshToken(refreshToken);
+        } else {
+            // db에 존재하지않으면 db에 저장
+            userRefreshToken = UserRefreshToken.builder()
+                    .user(user)
+                    .refreshToken(refreshToken)
+                    .build();
+        }
+        userRefreshTokenRepository.saveAndFlush(userRefreshToken);
+    }
+
+}
