fd7518812596e44a6b947dcc533d6903cece3575
HanHyunSoo <gustn4563@gmail.com>
2022-08-15 20:08:57 +0900

    feat : Change function name and Add parameter

--- src/main/java/com/debug/api/entity/user/User.java
+++ src/main/java/com/debug/api/entity/user/User.java
@@ -71,8 +71,11 @@ public class User extends BaseTimeEntity {
         }
     }
 
-    public void updateUserIdAndPassword(String userId, String password) {
+    public void changeUnconfirmed(String userId, String username, String password, String email) {
         this.userId = userId;
+        this.username = username;
         this.password = password;
+        this.email = email;
+        this.roleType = RoleType.UNCONFIRMED;
     }
 }
